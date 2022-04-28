ec583aac9a94766b778a5f655724377e62ea4acf
SuitDream <1843512349@qq.com>
2022-04-28 18:03:52 +0800

    打印函数的使用练习！

---
+++ test_22_04_08/test22_04_09.c
@@ -0,0 +1,27 @@
+
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//		scandf("%d", &a);
+//		printf("%d\n", a);
+//			return 0;
+//
+//}
+//int main()
+//{
+//	char arr[5] = { 0 };
+//	scanf("%s", arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a', 'b', 'c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
---
+++ test_22_04_08/你要好好学习吗.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	int input = 0;
+//	printf("jia ru bi te\n");
+//	printf("Do you want work hardly?(1/0)>:");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("Great offer!\n");
+//	else
+//		printf("Sell potato!\n");
+//}
+int main()
+{
+	int line = 0;
+	printf("join the bit\n");
+	while (line<1000)
+	{
+		printf("knock the oder: %d\n", line);
+		line++;
+	}
+	if (line >= 1000)
+		printf("great offer\n");
+	return 0;
+}
